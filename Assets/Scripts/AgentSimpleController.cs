2b222f1dae35266bc0c7061ee33f5ca8d1c4124d
Diego Eduardo Apeña De La Cruz <diego.apena@tecsup.edu.pe>
2026-04-09 12:06:46 -0500

    lab03

---
+++ Assets/Scripts/AgentSimpleController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.AI;
+public class AgentSimpleController : MonoBehaviour
+{
+    public Transform Target;
+    private NavMeshAgent agent;
+    
+    Transform player;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        GetComponent<CapsuleCollider>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (agent != null)
+        {
+            agent.SetDestination(Target.position);
+        }
+    }
+
+    // Detecta la colisión con el Target
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == Target)
+        {
+            Destroy(gameObject); // Destruye el agente
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        if (player != null)
+        {
+            Gizmos.color = Color.red;
+
+            Vector3 dir = player.position - transform.position;
+            Gizmos.DrawRay(transform.position, dir);
+        }
+    }
+}
